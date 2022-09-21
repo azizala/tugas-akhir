c26d6cd81db70f98a68b130b5c917936ac752c6b
Amir Aziz Al Awwabin <97922336+azizala@users.noreply.github.com>
2022-09-21 18:27:35 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php  
+//membuat koneksi ke database  
+
+	$host 		= 'localhost:3307';  
+  	$user 		= 'root';        
+  	$password 	= '';        
+  	$database 	= 'sbp';    
+      
+  $konek_db = mysqli_connect($host, $user, $password, $database);
+        
+?>
